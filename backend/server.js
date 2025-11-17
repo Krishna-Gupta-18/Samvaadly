0ef1357cccd2ce3ae2d8d34aff0a6946e5b0c969
Krishna Gupta <krishnasunitagupta18@gmail.com>
2025-11-17 18:54:59 +0530

    removed dummy api

--- backend/server.js
+++ backend/server.js
@@ -16,10 +16,6 @@ const io = socketIo(server, {
 });
 const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-  res.send('Backend is running!');
-});
-
 
 // Middleware
 app.use(cors());
@@ -218,5 +214,6 @@ io.on('connection', (socket) => {
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
 
 
