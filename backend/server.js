4bf9e8b7d0de4e0dfdbacbe90ecb47e734592cd7
Krishna Gupta <krishnasunitagupta18@gmail.com>
2025-11-17 18:51:13 +0530

    added dummy api to test backend
    Added a root route to confirm server is running.

--- backend/server.js
+++ backend/server.js
@@ -16,6 +16,11 @@ const io = socketIo(server, {
 });
 const PORT = process.env.PORT || 5000;
 
+app.get('/', (req, res) => {
+  res.send('Backend is running!');
+});
+
+
 // Middleware
 app.use(cors());
 app.use(express.json());
@@ -213,4 +218,5 @@ io.on('connection', (socket) => {
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
 
