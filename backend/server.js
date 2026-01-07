3d6d583eefed44c14ad5d4b08584d6be848ea56c
Krishna Gupta <krishnasunitagupta18@gmail.com>
2026-01-07 18:13:04 +0530

    Fix MongoDB connection syntax error

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ app.use(cors());
 app.use(express.json());
 
 // MongoDB connection
-mongoose.connect(process.env.MONGO_URI || 'mongodb+srv://Krishna:Cloud07@cluster0.adgwxrs.mongodb.net/?appName=Cluster0', {
+mongoose.connect(process.env.MONGO_URI){
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
@@ -215,6 +215,7 @@ io.on('connection', (socket) => {
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
 
 
 
